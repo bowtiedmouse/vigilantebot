09422290400ee278d139466967ebba3a5554dc4e
BowTiedMouse <bowtiedmouse@protonmail.com>
2022-02-03 19:36:53 +0000

    Refactored key updating/creating

--- vigilante/fileutils.py
+++ vigilante/fileutils.py
@@ -11,15 +11,10 @@ def create_empty_key_in_file(file: str, key: str):
     if not os.path.isfile(file) or os.stat(file).st_size == 0:
         create_empty_json_file(file)
 
-    with open(file, "r") as f:
-        data = json.load(f)
-    if key not in data:
-        data[key] = {}
-    with open(file, "w") as f:
-        json.dump(data, f)
+    update_file_key(file, key, {})
 
 
-def update_file_key(file: str, primary_key: str, secondary_key: str, content):
+def update_file_key(file: str, primary_key: str, content, secondary_key: str = ""):
     try:
         with open(file, "r") as f:
             file_data = json.load(f)
@@ -27,7 +22,10 @@ def update_file_key(file: str, primary_key: str, secondary_key: str, content):
         if primary_key not in file_data:
             file_data[primary_key] = {}
 
-        file_data[primary_key][secondary_key] = content
+        if not secondary_key:
+            file_data[primary_key] = content
+        else:
+            file_data[primary_key][secondary_key] = content
 
         with open(file, "w") as f:
             json.dump(file_data, f)
@@ -37,22 +35,27 @@ def update_file_key(file: str, primary_key: str, secondary_key: str, content):
         raise
 
 
-def get_file_key_content(file: str, primary_key: str, secondary_key: str = "") -> dict:
+def get_file_key_content(file: str, primary_key: str, secondary_key: str = ""):
     key_content = {}
 
     try:
-        with open(file, "r") as f:
-            saved_data = json.load(f)
-            key_content = saved_data[primary_key]
-            if secondary_key:
-                key_content = key_content[secondary_key]
+        key_content = _get_key_content(file, primary_key, secondary_key)
     except FileNotFoundError:
         print(f"File not found: {file}. Creating it...")
         create_empty_json_file(file)
     except KeyError:
-        print(f"Not found saved record for {primary_key}. Creating an empty one.")
-        create_empty_key_in_file(file, primary_key)
+        print(f"{primary_key} not found in {file}.")
     except json.decoder.JSONDecodeError:
         print("File is empty or malformed.")
+
+    return key_content
+
+
+def _get_key_content(file: str, primary_key: str, secondary_key: str):
+    with open(file, "r") as f:
+        saved_data = json.load(f)
+        key_content = saved_data[primary_key]
+        if secondary_key:
+            key_content = key_content[secondary_key]
 
     return key_content
