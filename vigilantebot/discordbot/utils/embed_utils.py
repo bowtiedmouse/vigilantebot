05c5bdd2819e4f730c749971ccfbce0ac991d69c
BowTiedMouse <bowtiedmouse@protonmail.com>
2022-02-12 01:02:36 +0000

    Some restructure

--- vigilantebot/discordbot/utils/embed_utils.py
+++ vigilantebot/discordbot/utils/embed_utils.py
@@ -5,7 +5,25 @@ import discord
 import vigilante
 
 
-def format_log(alerts_log):
+def format_log(alerts_log) -> dict:
+    """
+    Format a list of SortableAlert items into a plain dict.
+    :param alerts_log: [SortableAlert]
+
+    End format:
+    updates = {
+        'Whale 1': {
+            'alias': 'Whale 1',
+            'address': '0xa9999...',
+            'balance': 'Total USD balance: $660,005',
+            'content': '· Token removed: **WETH** (on *eth*). Previous balance: 2.598634407...'},
+        'Whale 2': {
+            'alias': 'Whale 2',
+            'address': '0xe1111',
+            'balance': 'Total USD balance: $616,802',
+            'content': '· New token: **CVXFXS** (on *eth*). Balance: 1558.8046700736468 (5.4% ...'}
+    }
+    """
     updates = {}
     last_alias = ''
 
@@ -24,10 +42,15 @@ def format_log(alerts_log):
             continue
 
         updates[alert.target_alias]['content'] += '· ' + alert.log() + '\n'
+
     return updates
 
 
-def create_embed(target_updates: dict):
+def create_update_report_embed(target_updates: dict) -> discord.Embed:
+    """
+    Creates an Embed with the data of the updates on one target.
+    :param target_updates: dict with the content of that target's updates.
+    """
     embed = discord.Embed(
         title=f"{target_updates['balance']}",
         color=discord.Color.random(seed=target_updates['address']),
@@ -37,6 +60,7 @@ def create_embed(target_updates: dict):
         name=target_updates['alias'],
         url=f"https://debank.com/profile/{target_updates['address']}",
         icon_url=f'https://identicon-api.herokuapp.com/{target_updates["address"]}/100?format=png')
+    # If identicon-api shuts down, alternative is utils.blockies.get_blockie()
 
     embed.add_field(
         name="Last wallet movements:",
