d4b3d7677ce7e0c68df317eb267c9a145f0ad709
BowTiedMouse <bowtiedmouse@protonmail.com>
2022-02-16 18:39:43 +0000

    using guild_config

--- vigilantebot/discordbot/discord_settings.py
+++ vigilantebot/discordbot/discord_settings.py
@@ -1,17 +1,26 @@
 import os
 
-from vigilante import get_targets_alias_list
+import guild_config
 
-GUILD_ID = int(os.getenv('DISCORD_GUILD_ID'))
-DEBUG_GUILD_IDS = [int(guild_id) for guild_id in os.getenv('DEBUG_GUILD_IDS')]
-# ALLOWED_CHANNELS = [int(channel) for channel in os.getenv('DISCORD_ALLOWED_CHANNELS').split(',')]
-ADMIN_ROLES = os.getenv('DISCORD_ALLOWED_ROLES').split(',')
-WATCH_FREQUENCY_SECONDS = 4 * 60 + 55
+# from .env
+# GUILD_ID = int(os.getenv('DISCORD_GUILD_ID'))
+# DEBUG_GUILD_IDS = [int(guild_id) for guild_id in os.getenv('DEBUG_GUILD_IDS')]
+# # ALLOWED_CHANNELS = [int(channel) for channel in os.getenv('DISCORD_ALLOWED_CHANNELS').split(',')]
+# ADMIN_ROLES = os.getenv('DISCORD_ALLOWED_ROLES').split(',')
+# WATCH_FREQUENCY_SECONDS = 4 * 60 + 55
 TOKEN = os.getenv('DISCORD_TOKEN')
+DEBUG_GUILD_IDS = [int(guild_id) for guild_id in os.getenv('DEBUG_GUILD_IDS').split(',')]
 
-GUILD_NAME = os.getenv('GUILD_NAME').replace(' ', '')
+# from config/<guild_id>.json
+# GUILD_ID = guild_config.get_guild_id()
+# ALLOWED_CHANNELS = guild_config.get_allowed_channels()
+# ADMIN_ROLES = guild_config.get_allowed_roles()
+ADMIN_ROLES = os.getenv('ALLOWED_ROLES').split(',')
+
+WATCH_FREQUENCY_SECONDS = 4 * 60 + 55
+
+# GUILD_NAME = os.getenv('GUILD_NAME').replace(' ', '')
+GUILD_NAME = guild_config.get_guild_name()
 SUBSCRIPTIONS_FILE = f"data/subscriptions_{GUILD_NAME}.json"
 PYCORD_LOG_FILE = f"logs/discord_{GUILD_NAME}.log"
 DISCORDBOT_LOG_FILE = f"logs/discordbot_{GUILD_NAME}.log"
-
-TARGETS_LIST = get_targets_alias_list()
--- vigilantebot/vigilante/settings.py
+++ vigilantebot/vigilante/settings.py
@@ -1,4 +1,5 @@
-import os
+import guild_config
+
 
 # Min token's USD amount to consider it a holding (to avoid dust left or small airdrops)
 MIN_TOKEN_BALANCE = 300
@@ -8,8 +9,9 @@ MIN_TOKEN_BALANCE = 300
 MIN_PC_DIFF = 5
 MIN_PORTFOLIO_PC = 0.49
 
-GUILD_ID = os.getenv('DISCORD_GUILD_ID')
-GUILD_NAME = os.getenv('GUILD_NAME').replace(' ', '')
+# GUILD_ID = os.getenv('DISCORD_GUILD_ID')
+# GUILD_NAME = os.getenv('GUILD_NAME').replace(' ', '')
+GUILD_NAME = guild_config.get_guild_name()
 TARGET_ACCOUNTS_FILE = f"target_accounts_{GUILD_NAME}.json"
 TARGETS_HOLDINGS_FILE = f"data/targets_holdings_{GUILD_NAME}.json"
 ALERTS_LOG_FILE = F"data/log_{GUILD_NAME}"
@@ -19,4 +21,5 @@ GAS_TOKENS = ['ETH', 'FTM', 'AVAX']
 # optional: &chain_id=eth
 DEBANK_TOKEN_LIST_URL = "https://openapi.debank.com/v1/user/token_list?id={address}&is_all=false"
 DEBANK_ACCOUNT_BALANCE_URL = "https://openapi.debank.com/v1/user/total_balance?id={address}"
-EXCLUDED_CHAINS = ["root['" + chain + "']" for chain in os.getenv('EXCLUDED_CHAINS').split(',')]
+# EXCLUDED_CHAINS = ["root['" + chain + "']" for chain in os.getenv('EXCLUDED_CHAINS').split(',')]
+EXCLUDED_CHAINS = ["root['" + chain + "']" for chain in guild_config.get_excluded_chains()]
